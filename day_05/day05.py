295fac6881e7f722151caed59a6e5361c6780755
reposefulGrass <reposefulGrass@gmail.com>
2022-04-07 13:45:41 -0500

    Refactor.

--- day_05/day05.py
+++ day_05/day05.py
@@ -1,6 +1,7 @@
 
 
 from dataclasses import dataclass
+from enum import Enum
 
 @dataclass
 class Line:
@@ -13,51 +14,64 @@ class Line:
         return f"({self.x1}, {self.y1}) -> ({self.x2}, {self.y2})"
 
 
+class LineType(Enum):
+    HORIZONTAL = 1
+    VERTICAL = 2
+    DIAGONAL = 3
+
+
 class Map:
     def __init__(self, size):
         self.ocean_floor = [['.' for _ in range(size)] for _ in range(size)]
 
-    def place_line(self, line: Line) -> None:
-
-        # vertical line
+    def determine_linetype(self, line: Line) -> LineType:
         if line.x1 == line.x2:
-            if line.y1 <= line.y2:
-                for y in range(line.y1, line.y2 + 1):
-                    self.update_spot(line.x1, y)
-            else:
-                for y in range(line.y2, line.y1 + 1):
-                    self.update_spot(line.x1, y)
-
-        # horizontal line
+            return LineType.HORIZONTAL
         elif line.y1 == line.y2:
-            if line.x1 <= line.x2:
-                for x in range(line.x1, line.x2 + 1):
-                    self.update_spot(x, line.y1)
-            else:
-                for x in range(line.x2, line.x1 + 1):
-                    self.update_spot(x, line.y1)
-
-        # This indicates a 45 degree angle
+            return LineType.VERTICAL
         elif abs(line.y2 - line.y1) == abs(line.x2 - line.x1):
-            for d in range(abs(line.x2 - line.x1) + 1):
-                if line.x1 <= line.x2:
-                    if line.y1 <= line.y2:
-                        self.update_spot(line.x1 + d, line.y1 + d)
-                    else:
-                        self.update_spot(line.x1 + d, line.y1 - d)
-                else:
-                    if line.y1 <= line.y2:
-                        self.update_spot(line.x1 - d, line.y1 + d)
-                    else:
-                        self.update_spot(line.x1 - d, line.y1 - d)
+            return LineType.DIAGONAL
+        else:
+            raise ValueError("Invalid line type.")
 
-    def update_spot(self, x, y):
+    def place_line(self, line: Line) -> None:
+        match self.determine_linetype(line):
+            case LineType.HORIZONTAL:
+                if line.y1 <= line.y2:
+                    for y in range(line.y1, line.y2 + 1):
+                        self.update_spot(line.x1, y)
+                else:
+                    for y in range(line.y2, line.y1 + 1):
+                        self.update_spot(line.x1, y)
+
+            case LineType.VERTICAL:
+                if line.x1 <= line.x2:
+                    for x in range(line.x1, line.x2 + 1):
+                        self.update_spot(x, line.y1)
+                else:
+                    for x in range(line.x2, line.x1 + 1):
+                        self.update_spot(x, line.y1)
+
+            case LineType.DIAGONAL:
+                for d in range(abs(line.x2 - line.x1) + 1):
+                    if line.x1 <= line.x2:
+                        if line.y1 <= line.y2:
+                            self.update_spot(line.x1 + d, line.y1 + d)
+                        else:
+                            self.update_spot(line.x1 + d, line.y1 - d)
+                    else:
+                        if line.y1 <= line.y2:
+                            self.update_spot(line.x1 - d, line.y1 + d)
+                        else:
+                            self.update_spot(line.x1 - d, line.y1 - d)
+
+    def update_spot(self, x: int, y: int) -> None:
         if self.ocean_floor[y][x] == '.':
             self.ocean_floor[y][x] = '1'
         else:
             self.ocean_floor[y][x] = str(int(self.ocean_floor[y][x]) + 1)
 
-    def compute_overlaps(self, num):
+    def compute_overlaps(self, num: int) -> int:
         overlaps = 0
         for row in self.ocean_floor:
             for col in row:
@@ -94,11 +108,11 @@ def parse_lines(lines: list[str]) -> list[Line]:
     return list_of_lines
 
 
-def find_dangerous_vents(lines: list[Line]):
-    map = Map(1000)
+def find_dangerous_vents(lines: list[Line], map_size: int, num_overlaps: int) -> int:
+    map = Map(map_size)
     for line in lines:
         map.place_line(line)
-    return map.compute_overlaps(2) 
+    return map.compute_overlaps(num_overlaps) 
     
 
 if __name__ == '__main__':
@@ -108,4 +122,6 @@ if __name__ == '__main__':
     lines = parse_lines(input)
 
     # Part 1 & 2
-    print(find_dangerous_vents(lines))
+    map_size = 1000
+    num_overlaps = 2
+    print(find_dangerous_vents(lines, map_size, num_overlaps))
